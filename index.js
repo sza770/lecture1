581cd4e563355a1d882eb27da23ae16d85312bc9
shneor <sza770@gmail.com>
2022-12-07 22:25:57 +0200

    updating

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { writeFile, readFile } from 'node:fs';
+import { writeFileSync, readFileSync } from 'node:fs';
 import { exit } from 'node:process';
 
 const me = {
@@ -6,18 +6,38 @@ const me = {
     lastName: "amramy",
     age: 34
 }
-writeFile("./me.json" , JSON.stringify(me), (err)=> {
-    if (err) {
-        console.log(err)
-        exit(1);
-    }
-    readFile("./me.json", (err, data) => {
+
+function fileSync () {
+    writeFileSync("./me.json" , JSON.stringify(me))
+    const data = readFile("./me.json")
+    console.log(data)
+}
+
+
+function fileAsync () {
+    writeFile("./me.json" , JSON.stringify(me), (err)=> {
         if (err) {
-            console.error(err);
-            exit(2);
+            console.log(err)
+            exit(1);
         }
-        console.log("%s", data)
-    
+        readFile("./me.json", (err, data) => {
+            if (err) {
+                console.error(err);
+                exit(2);
+            }
+            console.log("%s", data)
+        
+        })
     })
-})
-    
+}
+
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  readline.question('Who are you?', name => {
+    console.log(`Hey there ${name}!`);
+    readline.close();
+  });
